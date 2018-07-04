d21d2ba9df316979c8df67efdd7c6e40477f68c2
JorgeTB <jorgetomeb@gmail.com>
2018-07-04 23:40:39 +0200

    Readme, for more information check requirements.txt

--- ChallengeHolidog/settings.py
+++ ChallengeHolidog/settings.py
@@ -28,26 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
-#Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-EDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
-
-MEDIA_URL = '/media/'
-
-
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 
 # Application definition
@@ -63,7 +43,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'ChallengeHolidog.urls'
@@ -142,3 +122,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+EDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
+
+MEDIA_URL = '/media/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+try:
+    from holidog.local_settings import *
+except ImportError:
+    # No local settings was found, skipping.
+    pass
+
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # HolidogChallenge-
+
+On root directory execute:
+    python manage.py runserver [PORT] (example: 0.0.0.0:7000) 
